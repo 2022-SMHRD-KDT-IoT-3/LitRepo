29a145f8c32786bb2528c56b574a629b38d191f5
smhrd <smhrd@59.0.236.43>
2022-03-22 15:26:27 +0900

    커밋1

--- LITProject2/src/main/java/controller/Frontcontroller.java
+++ LITProject2/src/main/java/controller/Frontcontroller.java
@@ -1,6 +1,57 @@
 package controller;
 
-public class Frontcontroller {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Inter.Command;
+import service.WriteBoardService;
+
+@WebServlet("*.do")
+public class Frontcontroller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String nextPage = "";
+		Command com = null;
+		
+		System.out.println("[frontController 실행]");
+		
+		// 어떤 기능을 수행하는 문자열 인지 판단.
+		
+		String uri = request.getRequestURI();
+	
+		System.out.println(uri);
+		
+		String path = request.getContextPath();
+		System.out.println(path);
+		
+		String command = uri.substring(path.length() + 1);
+		
+		System.out.println(command);
+		
+		
+		
+		if(command.equals("WriteBoardService.do")) {
+				
+			com = new WriteBoardService();
+			nextPage = com.execute(request, response);
+			
+			
+		}
+		
+		
+		
+		
+		if(nextPage != null) {
+			response.sendRedirect(nextPage);
+		}
+	
+	}
 
 }
