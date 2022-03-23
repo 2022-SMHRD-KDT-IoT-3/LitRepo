c4d443c5351173895d656f3ee1b064b86de159bd
smhrd <smhrd@59.0.236.43>
2022-03-23 16:46:58 +0900

    LogoutServiceCon

---
+++ LITProject2/src/main/java/service/LogoutServiceCon.java
@@ -0,0 +1,26 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Inter.Command;
+
+public class LogoutServiceCon implements Command {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		
+		session.removeAttribute("info");
+		
+		
+		return "main1.jsp";
+	}
+
+}
